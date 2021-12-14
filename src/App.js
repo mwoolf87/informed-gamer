0a32effcc9ed0375123cf1837742cff6b60a799b
mwoolf87 <michael.j.woolf@gmail.com>
2021-12-14 18:12:08 -0500

    removed bootstrap.min file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./components/Navigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
